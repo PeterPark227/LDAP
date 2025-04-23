76cbf80535454cbb8f353ac1cc70655285af5a19
peterpark <peterpark@biztechi.com>
2025-04-23 09:23:05 +0900

    Initialize ldap project source package

---
+++ src/test/java/com/biztechi/ldap/LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biztechi.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
