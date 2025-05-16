d93657c4e61e1be7582ef2f905998b4c5d1bebfb
Paul <Kimch@biztechi.com>
2025-05-16 10:30:27 +0900

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+set -e
 
 echo "✅ .env 파일 다운로드"
 aws s3 cp s3://ldap-ci-secrets/.env /home/ubuntu/.env
-
-echo "📂 앱 디렉토리 생성 및 이동"
-mkdir -p /home/ubuntu/app
-cd /home/ubuntu/app
 
 echo "🧹 기존 컨테이너 정리"
 docker rm -f ldap-app || true
@@ -15,13 +12,11 @@ echo "🔐 ECR 로그인"
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 095215751727.dkr.ecr.ap-northeast-2.amazonaws.com
 
 echo "📦 이미지 pull"
+cd /home/ubuntu/ldap-app
 docker pull 095215751727.dkr.ecr.ap-northeast-2.amazonaws.com/ldap:latest
 
-echo "🚀 컨테이너 실행"
-docker run -d \
-  --name ldap-app \
-  --memory="512m" \
-  --cpus="0.5" \
-  --env-file /home/ubuntu/.env \
-  -p 8080:8080 \
-  095215751727.dkr.ecr.ap-northeast-2.amazonaws.com/ldap:latest
+echo "♻️ ldap-app 재시작"
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml up -d
+
+echo "=====================배포완료====================="
